c8e9754ac92dd047389f04d3428f9e02376d6817
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2021-09-03 13:58:07 +0200

    Add ecobulk wildcard domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(granta.api.ecobulk.upc.edu)
+domains=(ecobulk.upc.edu)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="angel.fernandez.sobrino@upc.edu" # Adding a valid address is strongly recommended
