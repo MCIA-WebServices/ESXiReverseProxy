1fbf30a0728c4ef1def15d7859732fb50a54aa74
Ángel Fernández Sobrino <71658542+AngelFernandezSobrino@users.noreply.github.com>
2021-12-09 15:18:18 +0100

    fix typo

--- src/delete-all.sh
+++ src/delete-all.sh
@@ -5,9 +5,9 @@ source config.sh
 for domain in ${domains[@]};
 do
     docker-compose run --rm --entrypoint "\
-        rm -Rf /etc/letsencrypt/live/domain && \
-        rm -Rf /etc/letsencrypt/archive/domain && \
-        rm -Rf /etc/letsencrypt/renewal/domain.conf" certbot
+        rm -Rf /etc/letsencrypt/live/$domain && \
+        rm -Rf /etc/letsencrypt/archive/$domain && \
+        rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
 done
 
 echo
