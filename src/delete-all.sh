f7d74aff21ad7fc7782896ce393a3154518f6684
Ángel Fernández Sobrino <angel.fernandez.sobrino@upc.edu>
2021-12-10 10:24:55 +0100

    Add warning to delete scripts

--- src/delete-all.sh
+++ src/delete-all.sh
@@ -1,13 +1,21 @@
-echo "### Deleting all certificates ..."
-
-source config.sh
-
-for domain in ${domains[@]};
-do
-    docker-compose run --rm --entrypoint "\
-        rm -Rf /etc/letsencrypt/live/$domain && \
-        rm -Rf /etc/letsencrypt/archive/$domain && \
-        rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
-done
-
+read -p "Are you sure, care with letsencrypt limits? (Y/y)" -n 1 -r
 echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Dangerous stuff
+
+    echo "### Deleting all certificates ..."
+
+    source config.sh
+
+    for domain in ${domains[@]};
+    do
+        docker-compose run --rm --entrypoint "\
+            rm -Rf /etc/letsencrypt/live/$domain && \
+            rm -Rf /etc/letsencrypt/archive/$domain && \
+            rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+    done
+    echo
+
+fi
--- src/delete.sh
+++ src/delete.sh
@@ -1,6 +1,14 @@
-echo "### Deleting certificate for $1 ..."
-docker-compose run --rm --entrypoint "\
-    rm -Rf /etc/letsencrypt/live/$1 && \
-    rm -Rf /etc/letsencrypt/archive/$1 && \
-    rm -Rf /etc/letsencrypt/renewal/$1.conf" certbot
+read -p "Are you sure, care with letsencrypt limits? (Y/y)" -n 1 -r
 echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Dangerous stuff
+    echo "### Deleting certificate for $1 ..."
+    docker-compose run --rm --entrypoint "\
+        rm -Rf /etc/letsencrypt/live/$1 && \
+        rm -Rf /etc/letsencrypt/archive/$1 && \
+        rm -Rf /etc/letsencrypt/renewal/$1.conf" certbot
+    echo
+
+fi
